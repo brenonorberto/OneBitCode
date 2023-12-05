b2dd661da6ea2211485a41799c3a0b4f4ac1f43b
Breno <brenonorberto@gmail.com>
2023-12-04 21:03:56 -0300

    [python] Adiciona o execicio final do modulo de intrudução ao python

--- python-introducao-python/exercicio_final.py
+++ python-introducao-python/exercicio_final.py
@@ -4,10 +4,18 @@ done = False
 # função para listar times
 
 
-def print_temas():
+def print_teams():
     print("Times listados:")
     for i, team in enumerate(teams.values()):
         print(f"{i+1} - {team['name']} ({len(team['players'])} jogadores)")
+
+# função para listar jogadores
+
+
+def print_team_players(team):
+    print(f"Jogadores do time {team['name']}")
+    for i, player in enumerate(team['players']):
+        print(f"{i+1}. {player}")
 
 
 while not done:
@@ -39,14 +47,44 @@ while not done:
         else:
             print("Número do time inválido!\n")
     elif choice == '3':
-        print_temas()
+        print_teams()
 
     elif choice == '4':
-        pass
+        print_teams()
+        team_num = int(
+            input("Informe o número do time que deseja adicionar o jogador?\n"))
+        if team_num <= len(teams):
+            team_name = list(teams.keys())[team_num - 1]
+            player_name = input("Digite o nome do jogador\n ")
+            teams[team_name]['players'].append(player_name)
+            print("Jogador adicionado com sucesso!\n")
+        else:
+            print("Número do time inválido!\n")
     elif choice == '5':
-        pass
+        print_teams()
+        team_num = int(
+            input("Informe o número do time que deseja remover o jogador?\n"))
+        if team_num <= len(teams):
+            team_name = list(teams.keys())[team_num - 1]
+            print_team_players(teams[team_name])
+            player_num = int(
+                input("Qual o número do jogador que deseja remover?\n"))
+            if player_num <= len(teams[team_name]['players']):
+                del teams[team_name]['players'][player_num - 1]
+                print("Jogador removido com sucesso!\n")
+            else:
+                print("Número do jogador inválido!\n")
+        else:
+            print("Número do time inválido!\n")
     elif choice == '6':
-        pass
+        print_teams()
+        team_num = int(
+            input("Informe o número do time que deseja listar os jogadores?\n"))
+        if team_num <= len(teams):
+            team_name = list(teams.keys())[team_num - 1]
+            print_team_players(teams[team_name])
+        else:
+            print("Número do time inválido!\n")
     elif choice == '7':
         done = True
     else:
