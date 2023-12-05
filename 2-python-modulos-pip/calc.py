82943d1182dd35d13cc5eed5d133abb0fd23c7f4
Breno <brenonorberto@gmail.com>
2023-12-05 18:00:14 -0300

    [python] Adiciona os primeiros arquivos do módulo

---
+++ 2-python-modulos-pip/calc.py
@@ -0,0 +1,18 @@
+def sum(a, b):
+    result = a + b
+    return result
+
+
+def sub(a, b):
+    result = a - b
+    return result
+
+
+def mul(a, b):
+    result = a * b
+    return result
+
+
+def div(a, b):
+    result = a / b
+    return result
